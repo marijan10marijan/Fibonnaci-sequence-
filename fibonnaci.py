2287e8c2b5e57db208747d16662ef6169f32dc72
marijan10marijan <eric_username.protonmail.com>
2022-01-10 20:38:58 +0100

    Creating fibonnaci sequence with function in Python

--- fibonnaci.py
+++ fibonnaci.py
@@ -1,7 +1,20 @@
-
-x = 21
-for i in range(x):
-    if i%2 == 0:
-        print(i)
+def fibonnaci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
     else:
-        pass
+        x = 0
+        y = 1
+        res = 0
+        for i in range(2,n+1):
+            res = x + y
+            x = y 
+            y = res
+        return res
+
+array = [fibonnaci(3),
+         fibonnaci(8),
+         fibonnaci(11)]
+
+print(array)
